d467220acaf21c23203b9da693b2ff16d98ff2b4
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-09-17 21:39:14 +0500

    amplify

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 // import { StatusBar } from 'expo-status-bar';
 import Main from "./Navigation/MainNavigation";
 import React from "react";
+import Amplify from "aws-amplify";
+import config from "./src/aws-exports";
+
+Amplify.configure(config);
 
 export default function App() {
   return <Main />;
