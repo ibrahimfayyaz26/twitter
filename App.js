648a8b3ed36087c549c74db73684f867ea4c0ea7
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-09-21 10:43:53 +0500

    aws

--- App.js
+++ App.js
@@ -2,10 +2,13 @@
 import Main from "./Navigation/MainNavigation";
 import React from "react";
 import Amplify from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
 import config from "./src/aws-exports";
 
 Amplify.configure(config);
 
-export default function App() {
+function App() {
   return <Main />;
 }
+
+export default withAuthenticator(App);
